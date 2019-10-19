b1e9eb986eb2eadc17e4d91dd514573adafca7c5
Andrei Shablyka <kloppp0007@gmail.com>
2019-10-19 22:48:51 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function zeros(expression) {
-  // your solution
+  debugger;
+  let res = {
+    twos: 0,
+    fives: 0,
+  }
+  expression.split('*').forEach(element => {
+    let num = parseInt(element);
+    if (element.includes('!!')) {
+      if (num % 2 === 0) {
+        for (let i = 2; i <= num; i *= 2) {
+          res.twos += Math.floor(num / i);
+        }
+        for (let i = 10; i <= num; i *= 5) {
+          res.fives += Math.floor(num / i);
+        }
+      } else for (let i = 5; i <= num; i *= 5) {
+        res.fives += Math.floor(num / i);
+      }
+    } else {
+    for (let i = 5; i <= num; i *= 5) {
+      res.fives += Math.floor(num / i);
+    }
+    for (let i = 2; i <= num; i *= 2) {
+      res.twos += Math.floor(num / i);
+    }
+  }
+  });
+  return Math.min(res.twos, res.fives);
 }
+
+//zeros('11!!*22!!*33!!');
